69e81b56a62a157c94041e0b581881fd4c9fdfb5
Jack21c <yakyu.airauto@yandex.ru>
2022-01-15 11:09:14 -0800

    p1

--- lab10.c
+++ lab10.c
@@ -1,94 +1,138 @@
 #include <stdio.h>
 #include <signal.h>
 #include <setjmp.h>
-#include <sys\stat.h>
+#include <sys/stat.h>
 #include <unistd.h>
+#include <limits.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define n 100
+
+typedef struct { /* Структура для хранения ирформации о файлах */
+  unsigned long blocks;
+  char filename[255];
+}fileinfo;
+
+fileinfo files[n]; /* Массив структур */
 
 sigjmp_buf obl; /* область памяти для запоминания
  состояния процесса */
-main(){
-    void prer(); /* подпрограмма обработки прерывания */
+int prerCount = 0; /* Cчётчик прерываний */
+int prerFlag = 0; /* флаг 5 прерываний */
+void prer(); /* подпрограмма обработки прерывания */
+unsigned long int totalBlockCount = 0;
+
+int main(){
+    int s;
+    int fileCount = 0; /* Счетчик количества файлов */
+    int file4blocksCount = 0; /* Счетчик количества файлов,
+    состоящих из более чем 4 блоков */
+    char pathName[PATH_MAX]; /*буфер, в который будет
+    помещен путь к текущей директории*/
+    fileinfo buffer;
+
     int end_flag; /* флаг завершения работы программы*/
     int descr[2]; /* дескрипторы межпроцессного канала */
-    signal(SIGINT, prer); /* уведомление о том, что 
-    в случае при¬хода сигнала прерывания SIGINT, 
+    signal(SIGINT, prer); /* уведомление о том, что
+    в случае прихода сигнала прерывания SIGINT,
     управление передается процедуре prer */
-    sigsetjmp(obl, 1); /* запоминание текущего 
-    состояния процесса */
-    end_flag = 0;
-    do{
-        sigsetjmp(obl, 1);
-        pipe(descr); /* создание межпроцессного канала */
-        if(fork() == 0) /* распараллеливание процесса */
-        {               /* процесс-потомок */
-            close(1);      /* закрытие стандартного вывода */    
-            close(descr[0]);   /* закрытие межпроцессного канала 
-            на чтение */ 
-            dup2(g[l],1);  /* дублирование дескриптора 
-            межпроцессного канала на стандартный вывод */ 
-            close(g[1]);   /* удалить копию */
-            execl("/bin/ps",  "ps",  "ax", 0); /* вывод всех 
-            активных процессов в системе в межпроцессный канал */
-
-
-        }
-        else 
-       {	      /* процесс-родитель */
-            wait(&s);      /* ожидание окончания процесса-потомка */ 
-            sigsetjmp(obl,1);
-            close(g[1]);
-            read(g[0], n, 80); /* считывание 1-ой записи из меж-процессного канала */
-            read(g[0], nl, 80); /* считывание 2-й записи */
-            rr = cmpstr(n, nl); /* сравнение двух строк */
-            sigsetjmp(obl,1);	
-            while (rr != -1)	
-            {
-                if(n1[7] == 'j') break; /* процесс управляется 
-                пользовательским терминалом */ 
-                m = atoi(nl); /* определение первого целого
-                        числа из строки nl */
-                if(m == 0) vv = 1;
-                if((gg == 0) && (m != 0)) printf ("%d\n", gg); 
-                if (gg != m) printf ("%d\n", m); 
-                gg = m;
-                read (g[0], n, 80);
-                rr = cmpstr(n, nl); 
-                strcpy(nl, n); 
-                sleep( 1);    /* ожидание 1 сек.  */
-		    }
-       }
-        while (vv != 1);	
-        sigsetjmp(obl,1);	
-        printf("good bye !!!\n");
+    for(int i = 0; i < n; i++){ /* Инициализируем структуру */
+        files[i].blocks = 0;
+        files[i].filename[0] = '\0';
     }
+    if(getcwd(pathName, sizeof(pathName)) != NULL){
+    /* Если удалось получить путь к директории */
+        sigsetjmp(obl, 1);
+        DIR *d; /* Инициализируем структуру DIR */
+        struct dirent *dir;
+        d = opendir(pathName); /* Открываем директорию по имени */
+        if(d){
+            while (dir = readdir(d)) {
+            /* Если директория открылась итерируем по ней до конца */
+                struct stat fileStat; /* Создаем структуру
+                статистики файла */
+                if(stat(dir->d_name, &fileStat) == -1){
+                /* Получаем статистику по файлу */
+                    perror("stat");
+                    return 1;
+                }
+                else if(dir->d_name[0]!='.'){
+                /* В случае успешного получения... */
+                    files[fileCount].blocks = fileStat.st_blocks; /* ...
+                    записываем размер в блоках... */
+                    strncpy(files[fileCount].filename, dir->d_name, 254);
+                    /* ...и имя файла */
+                    fileCount++; /*Считаем количество файлов*/
+                    totalBlockCount += fileStat.st_blocks; /* Считаем
+                    полное количество блоков */
+                    if(fileStat.st_blocks > 4){ /* Считаем количество
+                      файлов больше 4 блоков */
+                        file4blocksCount++;
+                    }
+                }
+            }
+            closedir(d); /* Закрываем директорию */
+            sigsetjmp(obl, 1);
+            pipe(descr); /* Создаем межпроцессный канал */
+            if(fork() == 0){ /* Распараллеливание процесса */
+            /* Процесс-потомок*/
+                close(descr[0]); /* Закрываем межпроцессный канал
+                на чтение... */
+                for(int i = 0; i < fileCount; i++){ /* ...и записываем
+                в него информацию */
+                    write(descr[1], &files[i].filename,
+                      sizeof(files[i].filename));
+                    write(descr[1], &files[i].blocks,
+                      sizeof(files[i].blocks));
+                }
+                exit(1);
+            }
+            else{
+                wait(&s); /* Ожидаем окончания процесса-потомка */
+                sigsetjmp(obl, 1);
+                close(descr[1]); /* Закрываем канал на запись */
+                for(int i = 0; i < file4blocksCount; i++){ /* Считываем
+                информацию из потока */
+                    read(descr[0], &buffer.filename,
+                      sizeof(buffer.filename));
+                    read(descr[0], &buffer.blocks,
+                      sizeof(buffer.blocks));
+                    if(buffer.blocks > 4){ /* Вывод файлов более 4 блоков */
+                        printf("%s: %lu\n",
+                          buffer.filename, buffer.blocks);
+                    }
+                    sigsetjmp(obl, 1);
+                }
+            }
+        }
+    }
+    else{
+        perror("getcwd() error");
+        return 1;
+    }
+    sleep(3);
+    sigsetjmp(obl, 1);
+    return 0;
+
 }
 /* Подпрограмма обработки прерывания */
-void po13() 
+void prer()
 {
-    ll ++;
-    signal (SIGINT, pol3);
-    if (11 > 9)	/* не больше 9ти прерываний */
-    {
-        printf ("good bye\n");
-        exit(l);
+    prerCount++;
+    if(prerCount%5 == 0){
+        prerFlag = 1;
+        printf("%lu\n", totalBlockCount);
     }
-    printf(" П P E P Ы В А Н И E   !!!  \n"); 
-    siglongjmp (obl, 1);    /* возвращение на последний     setjmp */
-}
-/* Подпрограмма сравнения строк */ 
-int cmpstr(vv,  nn)
-{
-    char vv[ ],  nn[ ];
-    int  i,  ml,  m2;
-    for (m1 = 0; vv[m1] != '\0'; ml ++); 
-    for (m2 = 0; vv[m2] != '\0'; m2 ++);
-    if (m1 != m2) return (0); /* длины строк vv и nn разные */ 
-    for (i = 0; i < ml; i ++)
-    {
-        if (vv[i] != nn[i]) return (i + 1); /* строки равны по
-        длине, но отличаются (i + 1) символом */
-        return(-1); /* строки одинаковы */
+    else{
+        prerFlag = 0;
     }
+    /*if(prerCount == 9){
+        sleep(3);
+        exit(0);
+    }*/
+    siglongjmp(obl, 1); /* возвращение на последний setjmp */
 }
-
-
