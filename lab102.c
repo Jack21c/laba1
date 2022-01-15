5a5f784052757901c452ed2848fe3e20dc6d12bd
Jack21c <yakyu.airauto@yandex.ru>
2022-01-15 14:36:04 -0800

    end 2
    final .c

--- lab102.c
+++ lab102.c
@@ -10,8 +10,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define n 100
-
 typedef struct { /* Структура для хранения ирформации о файлах */
   int blocks;
   char filename[255];
@@ -22,13 +20,16 @@ fileinfo buffer; /* Массив структур */
 sigjmp_buf obl; /* область памяти для запоминания
  состояния процесса */
 int prerCount = 0; /* Cчётчик прерываний */
-int prerFlag = 0; /* флаг 5 прерываний */
-void prer(int); /* подпрограмма обработки прерывания */
-unsigned long int totalBlockCount = 0;
+void prer(int sig); /* подпрограмма обработки прерывания */
+unsigned long long int totalBlockCount = 0; /* Общее количество блоков */
 
 int main(){
     int s;
-    struct sigaction sa;
+    struct sigaction sa; /* Создаём sigaction и заполняем поля */
+    sigset_t set;
+    sigemptyset(&set);
+    sigaddset(&set, SIGINT);
+    sa.sa_mask = set;
     sa.sa_flags = 0;
     sa.sa_handler = prer;
     sigaction(SIGINT, &sa, NULL);
@@ -37,33 +38,32 @@ int main(){
     int descr[2]; /* дескрипторы межпроцессного канала */
     int end_flag = 0; /* флаг завершения работы программы*/
     do{
+        sigsetjmp(obl, 1);
         pipe(descr); /* Создаем межпроцессный канал */
         if(fork() == 0){ /* Распараллеливание процесса */
         /* Процесс-потомок*/
             close(descr[0]); /* Закрываем межпроцессный канал
             на чтение... */
-            dup2(descr[1], 1);
-            //close(descr[1]);
-            getcwd(pathName, sizeof(pathName));
-            execl("/bin/du", "/bin/du", "-a", pathName, NULL);
+            dup2(descr[1], 1); /* Дублируем дескриптор межпроцессного канала
+            на стандартный вывод */
+            execlp("du", "a", NULL); /* вывод информации о каждом файле
+            текущего каталога в межпроцессный канал */
         }
         else{
             wait(&s); /* Ожидаем окончания процесса-потомка */
-            sigsetjmp(obl, 1);
             close(descr[1]); /* Закрываем канал на запись */
-            scanf("%d%s", &buffer.blocks, buffer.filename);
-            sigsetjmp(obl, 1);
-            while(strcmp(buffer.filename, "")){
+            dup2(descr[0], 0); /* Дублируем дескриптор межпроцессного канала
+            на стандартный ввод */
+            while(scanf("%d%s", &buffer.blocks, buffer.filename) != EOF){
+            /* Считываем информацию из межпроцессорного канала */
                 if(strcmp(buffer.filename, ".") == 0){
-                    totalBlockCount = buffer.blocks;
+                    totalBlockCount = buffer.blocks; /* Запоминаем общее
+                    количество блоков */
                     end_flag = 1;
-                    break;
                 }
-                if(buffer.blocks > 4){ /* Вывод файлов более 4 блоков */
+                else if(buffer.blocks > 4){ /* Выводим файлы более 4 блоков */
                     printf("%s: %d\n", buffer.filename, buffer.blocks);
                 }
-                scanf("%d%s", &buffer.blocks, buffer.filename);
-                sigsetjmp(obl, 1);
                 sleep(1);
             }
         }
@@ -77,17 +77,14 @@ void prer(int sig)
 {
     if(sig == SIGINT){
         prerCount++;
-        if(prerCount%5 == 0){
-            prerFlag = 1;
-            printf("%lu\n", totalBlockCount);
-        }
-        else{
-            prerFlag = 0;
+        if(prerCount%5 == 0){ /* Выводим общее количество блоков
+        при каждом пятом прерывании */
+            printf("%llu\n", totalBlockCount);
         }
         /*if(prerCount == 9){
             sleep(3);
             exit(0);
         }*/
     }
-    siglongjmp(obl, 1); /* возвращение на последний setjmp */
+    siglongjmp(obl, 1); /* возвращаемся на последний setjmp */
 }
